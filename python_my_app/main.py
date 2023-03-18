17d407e8c61621599a5d7d48bdf020b96462a2c1
pecolynx <pecolynx@gmail.com>
2023-03-19 01:46:28 +0900

    githubactions

--- python_my_app/main.py
+++ python_my_app/main.py
@@ -6,8 +6,8 @@ import pendulum
 logger = logging.getLogger(__name__)
 logger.info("INFO")
 
+a = os.getenv("A")
+print(a)
+
 now_in_paris = pendulum.now("Europe/Paris")
 print(now_in_paris)
-
-number = input("What is your favourite number?")
-print("It is", number + 1) 
