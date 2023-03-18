a677ac86d8c7946d7fb359499e1206332bcf8fa1
pecolynx <pecolynx@gmail.com>
2023-03-19 01:23:48 +0900

    githubactions

--- python_my_app/main.py
+++ python_my_app/main.py
@@ -3,11 +3,8 @@ import logging
 
 import pendulum
 
-
 logger = logging.getLogger(__name__)
 logger.info("INFO")
 
-a = os.getenv("A")
-print(a)
 now_in_paris = pendulum.now("Europe/Paris")
 print(now_in_paris)
