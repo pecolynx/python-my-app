cf2b10a81eb27add57126f5ec38c3a4951da21f1
pecolynx <pecolynx@gmail.com>
2023-03-19 00:08:01 +0900

    isort,mypy,flake8

--- python_my_app/main.py
+++ python_my_app/main.py
@@ -1,4 +1,10 @@
+import logging
+import os
+
 import pendulum
 
-now_in_paris = pendulum.now('Europe/Paris')
+now_in_paris = pendulum.now("Europe/Paris")
 print(now_in_paris)
+
+number = input("What is your favourite number?")
+print("It is", number + 1)  # error: Unsupported operand types for + ("str" and "int")
