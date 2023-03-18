ee32ce0801b41d788e05162fba734b4749a96afb
pecolynx <pecolynx@gmail.com>
2023-03-19 01:10:07 +0900

    githubactions

--- python_my_app/main.py
+++ python_my_app/main.py
@@ -3,10 +3,11 @@ import logging
 
 import pendulum
 
+
+logger = logging.getLogger(__name__)
+logger.info("INFO")
+
+a = os.getenv("A")
+print(a)
 now_in_paris = pendulum.now("Europe/Paris")
 print(now_in_paris)
-
-number = input("What is your favourite number?")
-print("It is", number + 1)  # error: Unsupported operand types for + ("str" and "int")
-
-a = 1 + 2
